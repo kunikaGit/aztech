f210f0e69e8f2cea1cd76ff983a021bf6979e252
HarryUpThere <harsh.chouhan010@gmail.com>
2025-06-13 00:39:44 +0530

    new code

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -52,7 +52,8 @@ const Services = () => {
                     list.map((category) => (
                         <div className='service-cards' onClick={(e) => { handleProduct(e, category) }}>
                             <div className='icon'>
-                                <img src={imageMap[`${category.icon}`]} alt='icon' />
+                                {/* <img src={imageMap[`${category.icon}`]} alt='icon' /> */}
+                                <img src={`${category.icon}`} alt='icon' />
                             </div>
                             <div className='content'>
                                 <h3>{category.name}</h3>

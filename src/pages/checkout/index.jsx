f7d054e3aea00b00d3b2b5cb69a0b2f885a25ecd
HarryUpThere <harsh.chouhan010@gmail.com>
2025-07-18 11:48:14 +0530

    merge code with search fix

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -11,9 +11,9 @@ const baseUrl = import.meta.env.VITE_BASE_URL;
 
 const Checkout = () => {
     let VITE_REACT_STRIPE_PUBLIC_KEY = import.meta.env.VITE_REACT_STRIPE_PUBLIC_KEY;
-        const stripePromise = loadStripe(VITE_REACT_STRIPE_PUBLIC_KEY); // Use your public key
+    const stripePromise = loadStripe(VITE_REACT_STRIPE_PUBLIC_KEY); // Use your public key
 
-        const [loading,setLoading]=useState(false)
+    const [loading, setLoading] = useState(false)
     const { fetchData } = useApiRequest();
     const navigate = useNavigate();
     const location = useLocation();
@@ -39,8 +39,8 @@ const Checkout = () => {
     useEffect(() => {
         if (!productDetails) {
             // Redirect if product details not found
-             navigate(`${baseUrl}`);
-           // callApi();
+            navigate(`${baseUrl}`);
+            // callApi();
 
         } else {
             callApi();
@@ -118,16 +118,16 @@ const Checkout = () => {
     };
 
     // ✅ Proceed to payment handler
-    const handleCheckout = async(e) => {
+    const handleCheckout = async (e) => {
         e.preventDefault()
         if (!validation()) return;
-setLoading(true)
-        const payload={...formData,paymentMethodId:selectedPayment.id,id:productDetails.id,plan_type:productDetails.id==4?"game":"plans",type_id}
+        setLoading(true)
+        const payload = { ...formData, paymentMethodId: selectedPayment.id, id: productDetails.id, plan_type: productDetails.id == 4 ? "game" : "plans", type_id }
         try {
             let res = await fetchData(API_ENDPOINTS.checkout, navigate, 'POST', payload);
 
             if (res.success) {
-            // Save session_id
+                // Save session_id
                 if (res?.data) {
                     localStorage.setItem("session_id", res.data);
                 }
@@ -142,97 +142,97 @@ setLoading(true)
                 }
 
             }
-              setLoading(false)
+            setLoading(false)
         } catch (error) {
-                setLoading(false)
+            setLoading(false)
 
             console.log(error)
         }
     };
     return (
         <>
-        <OverlayLoading isLoading={loading}/>
-        <div className='checkout-wrapped'>
-            <div className='dash-heading'>
-                <h2>Checkout</h2>
-            </div>
-            <div className='two-grid'>
-                <div className='left-side'>
-                    <div className='white-card'>
-                        <h3>Billing Details</h3>
-                        <form>
-                            <div className='input-main-data'>
-                                <label>First Name</label>
-                                <input type='text' placeholder='Enter First Name' name='first_name' value={formData.first_name}
-                                    onChange={handleInputChange} />
-                                {error.first_name && <div className='error-text'>{error.first_name}</div>}
-                            </div>
-                            <div className='input-main-data'>
-                                <label>Last Name</label>
-                                <input type='text' placeholder='Enter Last Name' name='last_name' value={formData.last_name}
-                                    onChange={handleInputChange} />
-                                {error.last_name && <div className='error-text'>{error.last_name}</div>}
-                            </div>
-                            <div className='input-main-data'>
-                                <label>Email</label>
-                                <input type='text' placeholder='Enter Email' name='email' value={formData.email}
-                                    onChange={handleInputChange} />
-                                {error.email && <div className='error-text'>{error.email}</div>}
-                            </div>
-                            <div className='input-main-data'>
-                                <label>Contact</label>
-                                <input type='text' placeholder='Enter contact number' name='contact_number' value={formData.contact_number}
-                                    onChange={handleInputChange} />
-                                {error.contact_number && <div className='error-text'>{error.contact_number}</div>}
-                            </div>
-                            <h3 className='mt-4'>Choose Payment Method</h3>
-                            {paymentMethods?.length > 0 &&
-                                paymentMethods.map((paymentType) => (
-                                    <div className='input-box checkcontainer'>
-                                        <label htmlFor='card'>
-                                            <input type='radio' id='card' name='payment' checked={selectedPayment?.id === paymentType.id}
-                                                onChange={() => handlePaymentChange(paymentType)} />{paymentType.name}
-                                        </label>
-                                        <img src={imageMap[`${paymentType.icon}`]} alt='cards' />
-                                    </div>
-                                ))}
-                            {error.payment_method && <div className='error-text'>{error.payment_method}</div>}
-
-                        </form>
-                    </div>
+            <OverlayLoading isLoading={loading} />
+            <div className='checkout-wrapped'>
+                <div className='dash-heading'>
+                    <h2>Checkout</h2>
                 </div>
+                <div className='two-grid'>
+                    <div className='left-side'>
+                        <div className='white-card'>
+                            <h3>Billing Details</h3>
+                            <form>
+                                <div className='input-main-data'>
+                                    <label>First Name</label>
+                                    <input type='text' placeholder='Enter First Name' name='first_name' value={formData.first_name}
+                                        onChange={handleInputChange} />
+                                    {error.first_name && <div className='error-text'>{error.first_name}</div>}
+                                </div>
+                                <div className='input-main-data'>
+                                    <label>Last Name</label>
+                                    <input type='text' placeholder='Enter Last Name' name='last_name' value={formData.last_name}
+                                        onChange={handleInputChange} />
+                                    {error.last_name && <div className='error-text'>{error.last_name}</div>}
+                                </div>
+                                <div className='input-main-data'>
+                                    <label>Email</label>
+                                    <input type='text' placeholder='Enter Email' name='email' value={formData.email}
+                                        onChange={handleInputChange} />
+                                    {error.email && <div className='error-text'>{error.email}</div>}
+                                </div>
+                                <div className='input-main-data'>
+                                    <label>Contact</label>
+                                    <input type='text' placeholder='Enter contact number' name='contact_number' value={formData.contact_number}
+                                        onChange={handleInputChange} />
+                                    {error.contact_number && <div className='error-text'>{error.contact_number}</div>}
+                                </div>
+                                <h3 className='mt-4'>Choose Payment Method</h3>
+                                {paymentMethods?.length > 0 &&
+                                    paymentMethods.map((paymentType) => (
+                                        <div className='input-box checkcontainer'>
+                                            <label htmlFor='card'>
+                                                <input type='radio' id='card' name='payment' checked={selectedPayment?.id === paymentType.id}
+                                                    onChange={() => handlePaymentChange(paymentType)} />{paymentType.name}
+                                            </label>
+                                            <img src={imageMap[`${paymentType.icon}`]} alt='cards' />
+                                        </div>
+                                    ))}
+                                {error.payment_method && <div className='error-text'>{error.payment_method}</div>}
 
-                {productDetails &&
-                    <div className='right-side'>
-                        <div className='white-card'>
-                            <h3>Order Details</h3>
-                            <div className='order-details-box'>
-                                <h6>Package Name: {productDetails.name}</h6>
-                                <p>Description: {productDetails.description}</p>
-                               {productDetails.duration && <span>Duration: {productDetails.duration}</span>}
+                            </form>
+                        </div>
+                    </div>
+
+                    {productDetails &&
+                        <div className='right-side'>
+                            <div className='white-card'>
+                                <h3>Order Details</h3>
+                                <div className='order-details-box'>
+                                    <h6>Package Name: {productDetails.name}</h6>
+                                    <p>Description: {productDetails.description}</p>
+                                    {productDetails.duration && <span>Duration: {productDetails.duration}</span>}
+                                </div>
                             </div>
-                        </div>
-                        <div className='white-card'>
-                            <table>
-                                <tr>
-                                    <th>Sub Total</th>
-                                    <td>$ {parseFloat(productDetails.amount).toFixed(2)}</td>
-                                </tr>
-                                {selectedPayment &&
+                            <div className='white-card'>
+                                <table>
                                     <tr>
-                                        <th>Fees Percentage</th>
-                                        <td>{selectedPayment.fees} %</td>
-                                    </tr>}
-                                <tr>
-                                    <th>Total</th>
-                                    <td>$ {parseFloat(finalPrice).toFixed(2)}</td>
-                                </tr>
-                            </table>
-                            <button type='button' className='blue-btn' onClick={(e) => handleCheckout(e)}>Proceed to payment</button>
-                        </div>
-                    </div>}
-            </div>
-        </div >
+                                        <th>Sub Total</th>
+                                        <td>$ {parseFloat(productDetails.amount).toFixed(2)}</td>
+                                    </tr>
+                                    {selectedPayment &&
+                                        <tr>
+                                            <th>Fees Percentage</th>
+                                            <td>{selectedPayment.fees} %</td>
+                                        </tr>}
+                                    <tr>
+                                        <th>Total</th>
+                                        <td>$ {parseFloat(finalPrice).toFixed(2)}</td>
+                                    </tr>
+                                </table>
+                                <button type='button' className='blue-btn' onClick={(e) => handleCheckout(e)}>Proceed to payment</button>
+                            </div>
+                        </div>}
+                </div>
+            </div >
         </>
     )
 }
